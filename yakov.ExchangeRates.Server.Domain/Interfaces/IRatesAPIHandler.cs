57843dda39155f1d0a9abb474217616163f7dd09
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-12-15 18:58:16 +0300

    Implemented NBRB API handler

---
+++ yakov.ExchangeRates.Server.Domain/Interfaces/IRatesAPIHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yakov.ExchangeRates.Server.Domain.Interfaces
+{
+    public interface IRatesAPIHandler
+    {
+        public Task<List<Currency>> GetAllCurrencies<Currency>();
+        public Task<List<Rate>> GetRatesByTimePeriod<Currency, Rate>(Currency currency, DateOnly dateStart, DateOnly dateEnd);
+    }
+}
---
+++ yakov.ExchangeRates.Server.Infrastructure/RemoteAPIHandlers/NBRBHandler.cs
@@ -0,0 +1,69 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using yakov.ExchangeRates.Server.Domain.Entities;
+using yakov.ExchangeRates.Server.Domain.Entities.RemoteAPIs;
+using yakov.ExchangeRates.Server.Domain.Interfaces;
+
+namespace yakov.ExchangeRates.Server.Infrastructure.RemoteAPIHandlers
+{
+    public class NBRBHandler : IRatesAPIHandler
+    {
+        public NBRBHandler()
+        {
+            _httpClient = new();
+            _httpClient.BaseAddress = new Uri(_nbrbUri);
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        private readonly HttpClient _httpClient;
+        private readonly string _nbrbUri = "https://www.nbrb.by/";
+
+        public async Task<List<Currency>> GetAllCurrencies<Currency>()
+        {
+            List<Currency> resultCurrencies = new(); 
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync("api/exrates/currencies");
+                if (response.IsSuccessStatusCode)
+                {
+                    var receivedCurrencies = await response.Content.ReadFromJsonAsync<List<Currency>>(); 
+                    resultCurrencies.AddRange(receivedCurrencies);
+                }
+            }
+            catch { }
+
+            return resultCurrencies;
+        }
+
+        public async Task<List<Rate>> GetRatesByTimePeriod<Currency, Rate>(Currency currency, DateOnly dateStart, DateOnly dateEnd)
+        {
+            List<Rate> resultRates = new();
+            CurrencyNBRB? currencyNBRB = currency as CurrencyNBRB;
+            try
+            {
+                string arguments = $"{currencyNBRB?.Cur_ID}" +
+                    $"?startDate={dateStart.ToString("yyyy-M-d")}" +
+                    $"&endDate={dateEnd.ToString("yyyy-M-d")}";
+
+                HttpResponseMessage response = await _httpClient.GetAsync("API/ExRates/Rates/Dynamics/" + arguments);
+                if (response.IsSuccessStatusCode)
+                {
+                    var receivedRates = await response.Content.ReadFromJsonAsync<List<Rate>>();
+                    resultRates.AddRange(receivedRates);
+                }
+            }
+            catch { }
+
+            return resultRates;
+        }
+
+        ~NBRBHandler()
+        {
+            _httpClient.Dispose();
+        }
+    }
+}
