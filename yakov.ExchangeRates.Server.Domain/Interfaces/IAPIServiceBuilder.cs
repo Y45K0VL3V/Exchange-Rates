97eb64abace6710f16f0ed86cbf3d7f54805ed65
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-12-15 23:01:06 +0300

    Added factory to create API handler services

---
+++ yakov.ExchangeRates.Server.Domain/Interfaces/IAPIServiceBuilder.cs
@@ -0,0 +1,9 @@
+﻿using yakov.ExchangeRates.Server.Domain.Entities;
+
+namespace yakov.ExchangeRates.Server.Domain.Interfaces
+{
+    public interface IAPIServiceBuilder
+    {
+        public IRatesAPIService BuildAPIService(CurrencyType currencyType);
+    }
+}
---
+++ yakov.ExchangeRates.Server.Infrastructure/RemoteAPIServices/RatesAPIServiceBuilder.cs
@@ -0,0 +1,23 @@
+﻿using yakov.ExchangeRates.Server.Domain.Entities;
+using yakov.ExchangeRates.Server.Domain.Interfaces;
+
+namespace yakov.ExchangeRates.Server.Infrastructure.RemoteAPIServices
+{
+    public class RatesAPIServiceBuilder : IAPIServiceBuilder
+    {
+        public IRatesAPIService BuildAPIService(CurrencyType currencyType)
+        {
+            switch (currencyType)
+            {
+                case CurrencyType.Fiat:
+                    return new NBRBService();
+
+                case CurrencyType.Crypto:
+                    return null;
+                
+                default: 
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
