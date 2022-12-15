f703d2a54fe3bbfa2a3e7300841409ec53f75303
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-12-15 17:12:11 +0300

    Add server entities * rate entity for self API * base entities for NBRB (Belarus National bank) API respones

---
+++ yakov.ExchangeRates.Server.Domain/Entities/Rate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yakov.ExchangeRates.Server.Domain.Entities
+{
+    public enum CyrrencyType
+    {
+        Fiat,
+        Crypto
+    }
+
+    public class Rate
+    {
+        public string CurrencySymbol { get; set; }
+        public CyrrencyType CyrrencyType { get; set; }
+        public DateOnly Date { get; set; }
+        public decimal Value { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
