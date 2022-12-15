e44be37bd174df1f80fbb7b544d1ec58ac46ba17
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-12-15 23:10:54 +0300

    Added rates context

---
+++ yakov.ExchangeRates.Server.Application/RatesContext.cs
@@ -0,0 +1,14 @@
+﻿using yakov.ExchangeRates.Server.Domain.Entities;
+
+namespace yakov.ExchangeRates.Server.Application
+{
+    public class RatesContext
+    {
+        public RatesContext()
+        {
+            Rates = new();
+        }
+
+        public Dictionary<Currency, Rate> Rates { get; set; }
+    }
+}
