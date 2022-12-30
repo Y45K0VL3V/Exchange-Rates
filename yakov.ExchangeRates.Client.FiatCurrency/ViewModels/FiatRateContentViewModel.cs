fc4d899caea7e46fa853ede38193881da915ad35
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-12-31 02:01:58 +0300

    Installed new LiveCharts version

--- yakov.ExchangeRates.Client.FiatCurrency/ViewModels/FiatRateContentViewModel.cs
+++ yakov.ExchangeRates.Client.FiatCurrency/ViewModels/FiatRateContentViewModel.cs
@@ -5,21 +5,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
 
 namespace yakov.ExchangeRates.Client.FiatCurrency.ViewModels
 {
     public class FiatRateContentViewModel : BindableBase
     {
-        private string _message;
-        public string Message
+        //public ISeries[] Series { get; set; } =
+        //{
+        //new LineSeries<ObservablePoint>
+        //{
+        //    Values = new ObservablePoint[]
+        //    {
+        //        new ObservablePoint(0, 4),
+        //        new ObservablePoint(1, 3),
+        //        new ObservablePoint(3, 8),
+        //        new ObservablePoint(18, 6),
+        //        new ObservablePoint(20, 12)
+        //    }
+        //}
+        //};
+        private readonly ObservableCollection<ObservableValue> _observableValues;
+
+        public FiatRateContentViewModel(ObservableCollection<ObservableValue> observableValues, ObservableCollection<ISeries> series)
         {
-            get { return _message; }
-            set { SetProperty(ref _message, value); }
+            _observableValues = new ObservableCollection<ObservableValue>
+            {
+                new ObservableValue(2),
+                new(5),
+                new(4),
+                new(5),
+                new(2),
+                new(6),
+                new(6),
+                new(6),
+                new(4),
+                new(2),
+                new(3),
+                new(4),
+                new(3)
+            };
+
+            Series = new ObservableCollection<ISeries>
+            {
+                new LineSeries<ObservablePoint>()
+                {
+                    Values = new ObservablePoint[]
+                    {
+                        new ObservablePoint(0, 4),
+                        new ObservablePoint(1, 3),
+                        new ObservablePoint(3, 8),
+                        new ObservablePoint(18, 6),
+                        new ObservablePoint(20, 12)
+                    },
+                    Fill = null
+                }
+            };
         }
 
-        public FiatRateContentViewModel()
-        {
-            Message = "View A from your Prism Module";
-        }
+        public ObservableCollection<ISeries> Series { get; set; }
+
     }
 }
