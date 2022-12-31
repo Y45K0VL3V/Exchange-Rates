276dda0056b97046853235752775f88143129920
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-12-31 18:34:10 +0300

    Implemented client's RatesService

--- yakov.ExchangeRates.Client.Services/Interfaces/IRatesService.cs
+++ yakov.ExchangeRates.Client.Services/Interfaces/IRatesService.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using yakov.ExchangeRates.Client.Business;
 
 namespace yakov.ExchangeRates.Client.Services.Interfaces
 {
     public interface IRatesService
     {
+        /// <summary>
+        /// Get all currencies by type
+        /// </summary>
+        /// <param name="currencyType">Type of currency, which is fiat or crypto</param>
+        /// <returns>List of currencies</returns>
+        public Task<List<Currency>> GetCurrencies(CurrencyType currencyType);
 
+        /// <summary>
+        /// Get rates of currency with specified time period
+        /// Longest period is 1 year
+        /// Earliest time border is 5 years from current date
+        /// </summary>
+        /// <param name="currency">Currency to search rates</param>
+        /// <param name="dateStart">Left time border (include)</param>
+        /// <param name="dateEnd">Right time border (include)</param>
+        /// <returns>Sorted by date rates list</returns>
+        public Task<List<Rate>> GetRates(Currency currency, DateOnly dateStart, DateOnly dateEnd);
     }
 }
--- yakov.ExchangeRates.Client.Services/RatesService.cs
+++ yakov.ExchangeRates.Client.Services/RatesService.cs
@@ -1,13 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using yakov.ExchangeRates.Client.Business;
+using yakov.ExchangeRates.Client.Services.Interfaces;
 
 namespace yakov.ExchangeRates.Client.Services
 {
-    public class RatesService
+    public class RatesService : IRatesService
     {
+        public RatesService()
+        {
+            _httpClient = new();
+            _httpClient.BaseAddress = new Uri(_serverUri);
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
 
+        private readonly HttpClient _httpClient;
+        private readonly string _serverUri = "https://localhost:7038/";
+
+        public Task<List<Currency>> GetCurrencies(CurrencyType currencyType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Rate>> GetRates(Currency currency, DateOnly dateStart, DateOnly dateEnd)
+        {
+            List<Rate> rates;
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync($"api/Rates?Type={currency.Type}&ShortName={currency.ShortName}" +
+                                                                          $"&dateStart={dateStart}" +
+                                                                          $"&dateEnd={dateEnd}");
+                if (response.IsSuccessStatusCode)
+                {
+                    rates = new(await response.Content.ReadFromJsonAsync<IEnumerable<Rate>>());
+                    return rates;
+                }
+                else
+                    return new();
+            }
+            catch
+            {
+                return new();
+            }
+        }
     }
 }
