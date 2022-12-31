c0868a63bde1f1650f60fbd6678d5611b6187457
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-12-31 18:10:39 +0300

    Implemented server models in client

--- yakov.ExchangeRates.Client.Business/Currency.cs
+++ yakov.ExchangeRates.Client.Business/Currency.cs
@@ -1,14 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace yakov.ExchangeRates.Client.Business
+﻿namespace yakov.ExchangeRates.Client.Business
 {
+    public enum CurrencyType
+    {
+        Fiat,
+        Crypto
+    }
 
     public class Currency
     {
+        public CurrencyType Type { get; set; }
+        public string ShortName { get; set; }
 
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Currency);
+        }
+
+        public bool Equals(Currency other)
+        {
+            return other != null &&
+                   Type == other.Type &&
+                   ShortName == other.ShortName;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Type, ShortName);
+        }
     }
 }
--- yakov.ExchangeRates.Client.Business/Rate.cs
+++ yakov.ExchangeRates.Client.Business/Rate.cs
@@ -1,7 +1,15 @@
-﻿namespace yakov.ExchangeRates.Client.Business
+﻿using System.Text.Json.Serialization;
+using yakov.ExchangeRates.Client.Business.Converters;
+
+namespace yakov.ExchangeRates.Client.Business
 {
     public class Rate
     {
+        public Currency Currency { get; set; }
 
+        [JsonConverter(typeof(DateOnlyJsonConverter))]
+        public DateOnly Date { get; set; }
+        public decimal Value { get; set; }
+        public decimal Amount { get; set; }
     }
 }
